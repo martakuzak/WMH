48ea24dbaeee075ce94c7f08faacb79f3a9730b9
Marta Kuzak <marta.kuzak@gmail.com>
2015-01-19 11:23:09 +0100

    czytanie

--- src/graph/GraphReader.java
+++ src/graph/GraphReader.java
@@ -8,24 +8,22 @@ import java.util.Scanner;
 public class GraphReader {
 	
 	public static int[][] readFile(int ilosc_wierz, int wag_min, int wag_max) throws FileNotFoundException {
-
-			File file = new File("output/"+ilosc_wierz+"_"+wag_min+"_"+wag_max+".txt"); //zmienilam ukosnik na / , bo tu (na Ubuntu) lecial FileNotFoundException. 
-																						//A z takim ukosnikiem zadzialalo mi na Windowsie, daj znac jak u Ciebie
-			Scanner scanner = new Scanner(file);
-			scanner.next(); // VERTEX_PART_NUMBER
-			int vpnumber = scanner.nextInt();
-			//System.out.println("Liczba wierzcho³ków grafu: " + 2 * vpnumber);
-			scanner.next(); //WEIGHT
-			int minWeight = scanner.nextInt();
-			int maxWeight = scanner.nextInt();
-		//	System.out.println("Zakres wag krawêdzi: " + minWeight + " - " + maxWeight);
-			//readEdges(scanner, vpnumber);
-			int [][] ttab = readEdges(scanner, vpnumber);
-			scanner.close();
-			return ttab;
-		
+		String fileName = new String("output/"+ilosc_wierz+"_"+wag_min+"_"+wag_max+".txt");
+		return readFile(fileName);
 	}
 
+	public static int[][] readFile(String fileName) throws FileNotFoundException {
+		File file = new File(fileName); 
+		Scanner scanner = new Scanner(file);
+		scanner.next(); // VERTEX_PART_NUMBER
+		int vpnumber = scanner.nextInt();
+		scanner.next(); //WEIGHT
+		int minWeight = scanner.nextInt();
+		int maxWeight = scanner.nextInt();
+		int [][] ttab = readEdges(scanner, vpnumber);
+		scanner.close();
+		return ttab;
+	}
 	
 	
 	public static int[][] readEdges(Scanner scanner, int vpnumber) {
